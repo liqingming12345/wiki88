3430db5cad94e4d942eda5909f6559467704f12f
apple-lab <apple>
2022-01-25 07:34:02 +0800

    2-5 修改启动文案

---
+++ src/main/java/com/jiawa/wike88/Wiki88Application.java
@@ -0,0 +1,19 @@
+package com.jiawa.wike88;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class Wiki88Application {
+	private static final Logger LOG = LoggerFactory.getLogger(Wiki88Application.class);
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Wiki88Application.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+	}
+
+}
