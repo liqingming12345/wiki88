77c5a4ad6a945d49244d25717678f0d56f20d4df
apple-lab <apple>
2022-01-24 15:11:24 +0800

    2-2 init

---
+++ src/main/java/com/jiawa/wike88/Wike88Application.java
@@ -0,0 +1,13 @@
+package com.jiawa.wike88;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Wike88Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Wike88Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiawa/wike88/Wike88ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiawa.wike88;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Wike88ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
