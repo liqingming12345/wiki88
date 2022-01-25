e1d2f10dda83981b3223395da0d445d84322f5b1
apple-lab <apple>
2022-01-25 18:26:24 +0800

    2-8 增加自定义配置项示例

--- src/main/java/com/jiawa/wike88/controller/Testcontroller.java
+++ src/main/java/com/jiawa/wike88/controller/Testcontroller.java
@@ -1,23 +1,28 @@
 package com.jiawa.wike88.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 public class Testcontroller {
 
-    /**
-     * GET, POST, PUT, DELETE
-     *
-     * /user?id=1
-     * /user/1
-     * @return
-     */
+    @Value("${test.hello:TEST}")
+    private String testHello;
+
+
    /* @PostMapping
     @PutMapping
     @DeleteMapping
     @RequestMapping(value = "/user/1",method = RequestMethod.GET)*/
     @GetMapping("/hello")
     public String hello(){
-        return "hello JUN";
+        return "hello JUN" + testHello;
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello JUN!Post," + name;
     }
 }
