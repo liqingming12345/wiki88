3af000cd9ba53696c8b98d1ac85775ad0ff85fac
apple-lab <apple>
2022-01-28 11:06:07 +0800

    5-2 使用axios调用后端接口/ebook/list

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -46,8 +46,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios from 'axios';
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  setup(){
+    console.log();
+    axios.get("http://localhost:8880/ebook/list?name=Spring").then((response)=>{
+      console.log(response);
+    })
+  }
 });
 </script>
