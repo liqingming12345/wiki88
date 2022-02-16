f8a991a250c9ef4e19722c0c994cc3439e4e7a7e
apple-lab <apple>
2022-02-16 13:06:31 +0800

    10-5 定时任务示例

--- src/main/java/com/jiawa/wiki88/config/Wiki88Application.java
+++ src/main/java/com/jiawa/wiki88/config/Wiki88Application.java
@@ -5,12 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @ComponentScan("com.jiawa")
 @SpringBootApplication
 @MapperScan("com.jiawa.wiki88.mapper")
+@EnableCaching
 public class Wiki88Application {
 	private static final Logger LOG = LoggerFactory.getLogger(Wiki88Application.class);
 	public static void main(String[] args) {
