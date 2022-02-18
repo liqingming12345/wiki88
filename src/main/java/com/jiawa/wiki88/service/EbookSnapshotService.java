76094c6043197cd8d409cc3a6407d6c29c73d673
apple-lab <apple>
2022-02-18 08:58:28 +0800

    11-6 后端增加获取统计数值接口

--- src/main/java/com/jiawa/wiki88/service/EbookSnapshotService.java
+++ src/main/java/com/jiawa/wiki88/service/EbookSnapshotService.java
@@ -1,8 +1,10 @@
 package com.jiawa.wiki88.service;
 
 import com.jiawa.wiki88.mapper.EbookSnapshotMapperCust;
+import com.jiawa.wiki88.resp.StatisticResp;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class EbookSnapshotService {
@@ -13,4 +15,11 @@ public class EbookSnapshotService {
     public void genSnapshot() {
         ebookSnapshotMapperCust.genSnapshot();
     }
+
+    /**
+     +     * 获取首页数值数据：总阅读数、总点赞数、今日阅读数、今日点赞数、今日预计阅读数、今日预计阅读增长
+     +     */
+    public List<StatisticResp> getStatistic() {
+                return ebookSnapshotMapperCust.getStatistic();
+            }
 }
